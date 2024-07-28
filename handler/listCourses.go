94b9b27ef4f32c8fcd1ffd5f0ea3abf0576de4fb
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-07-28 11:49:28 -0300

    feat: list courses endpoint

--- handler/listCourses.go
+++ handler/listCourses.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/isaacmirandacampos/gocourses/models"
 )
 
 func ListCourse(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
+	courses := []models.Course{}
+	if err := db.Find(&courses).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "failed to get courses")
+		return
+	}
+	sendSuccess(ctx, http.StatusOK, "courses found", courses)
 }
