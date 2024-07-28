d0b29b85dd111d7bd69ec81264b5c345505628f7
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-07-28 11:08:48 -0300

    feat: Improve error message

--- handler/response.go
+++ handler/response.go
@@ -3,5 +3,5 @@ package handler
 import "github.com/gin-gonic/gin"
 
 func sendError(ctx *gin.Context, statusCode int, msg string) {
-	ctx.JSON(statusCode, gin.H{"message": msg})
+	ctx.JSON(statusCode, gin.H{"errorCode": statusCode, "message": msg})
 }
